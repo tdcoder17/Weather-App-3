0966be75b93f350a6339dfec6ac939dd0a0bb5c5
tdcoder17 <57919329+tdcoder17@users.noreply.github.com>
2022-01-06 13:16:42 -0600

    Initial commit

---
+++ Weather_App_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_3App.swift
+//  Weather App 3
+//
+//  Created by Theo Davis on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
